c9f8ad99c1c8a593d22993df9648152bec353c4d
danik94 <44891380+danik94@users.noreply.github.com>
2018-12-07 12:53:05 +0100

    passwort_vergessen_do_daniel

--- functions/users/login_form.php
+++ functions/users/login_form.php
@@ -12,7 +12,7 @@
 <div id="loginbox">
     <div id="loginheader">ally</div>
     <div id="login"
-        <form id="formular" action="../../functions/users/login_do.php" method="post">
+        <form id="formular" action="login_do.php" method="post">
             <div id="ueberschrift">Login</div>
             <div id="linie_anfang"></div>
             <input type="text" id="username" name="username" placeholder="Username"><br>
@@ -20,7 +20,7 @@
             <input type="checkbox" id="login_merken" name="merken" value="merken">
             <div id="login_merken_text">Login merken</div>
             <a href="passwort_vergessen.php" id="passwort_vergessen">Passwort vergessen</a>
-            <button class="button">Anmelden</button>
+            <button type="submit" class="button">Anmelden</button>
             <div id="linie_ende"></div>
         </form>
     </div>
