e49a873dfea85819761f5d733b29b0d347d6f56c
danik94 <44891380+danik94@users.noreply.github.com>
2018-12-07 12:47:19 +0100

    passwort_vergessen_do_daniel

--- functions/users/passwort_vergessen.php
+++ functions/users/passwort_vergessen.php
@@ -12,12 +12,12 @@
 <div id="loginbox">
     <div id="loginheader">ally</div>
     <div id="login">
-        <form id="formular">
+        <form id="formular" action="passwort_vergessen_do.php" method="post">
             <div id="ueberschrift">Passwort vergessen</div>
             <div id="linie_anfang"></div>
             <input type="text" id="email" name="email" placeholder="Email"><br>
             <input type="text" id="username" name="username" placeholder="Username"><br>
-            <button class="button">Passwort neu anfordern</button>
+            <button type="submit" class="button">Passwort neu anfordern</button>
             <div id="linie_ende"></div>
         </form>
     </div>
