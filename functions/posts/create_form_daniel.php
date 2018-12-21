56f42619e0bf4031ebc110cfbcc470bc1ac7b47d
theresa <ts175@hdm-stuttgart.de>
2018-12-21 08:59:59 +0100

    create

---
+++ functions/posts/create_form_daniel.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+<form action="create_do_daniel.php" method="post" enctype="multipart/form-data">
+<input type="file" name="datei"><br>
+<input type="submit" value="Hochladen">
+</form>
+</body>
+</html>
