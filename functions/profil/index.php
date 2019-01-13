3208f836825b7ecc6731231eede9aef5858bc6af
carinabinsfeld <44891379+carinabinsfeld@users.noreply.github.com>
2019-01-13 12:34:37 +0100

    index

--- functions/profil/index.php
+++ functions/profil/index.php
@@ -3,6 +3,9 @@ switch ($_GET["action"]){
     case"edit":
         include"./functions/profil/profil_form.php";
         break;
+    case"follow":
+        include"./functions/profil/follow_do.php";
+        break;
     default:
         include"./functions/profil/profil.php";
 };
