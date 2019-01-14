eba625af27059e7b03bc77892d6acb04e22b34eb
theresa <ts175@hdm-stuttgart.de>
2019-01-14 14:45:51 +0100

    update

--- functions/profil/profil_form.php
+++ functions/profil/profil_form.php
@@ -9,9 +9,9 @@ $db = null;
 while ($zeile = $statement->fetchObject()) {
     echo "<form action='./functions/profil/profil_do.php' method='post' enctype='multipart/form-data'>";
     echo "<input type='text' name='full_name' placeholder='Name' value='$zeile->name'/>"; //wird aus der db ausgelesen
-    echo "<textarea name='bio' placeholder='bio' rows='10'>$zeile->bio</textarea>";
+    echo "<textarea name='Bio' placeholder='Über mich' rows='10'>$zeile->bio</textarea>";
     echo "<input type='file' name='datei'>";
-	echo "<input type='submit' class='button' value='Bearbeiten'>";
+	echo "<input type='submit' class='button' value='speichern'>";
     echo "</form>";
 }
 ?>
