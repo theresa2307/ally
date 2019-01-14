49059ecc8d13c4d4159658dc813218e716d4a26d
theresa <ts175@hdm-stuttgart.de>
2019-01-14 14:05:52 +0100

    update

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!doctype html>
 <html lang="de">
 <head>
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
         <!-- Einbindung Stylesheet-->
-        <link href="ally.css" rel="stylesheet">
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.mim.js"></script>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 
@@ -16,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
     <meta charset="utf-8">
-    <title>Beispiel</title>
+    <title>ally</title>
 </head>
 <body>
 <?php
